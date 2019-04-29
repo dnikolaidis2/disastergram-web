cf2764c654527b0f320ce0ba367f24a3c68666c6
Dimitrris kastrinakis <dimitriskas.1997@gmail.com>
2019-04-29 16:53:41 +0300

    [WIP] Sidebar first implementation

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -26,13 +26,19 @@ export default class UserPage extends React.Component {
 		const isLoggedIn = this.props.isLoggedIn;
 		return (
 			<div className='userpage'>
-				<FlexBox>
-					<h1>You are logged in!</h1>
-					{isLoggedIn
-						? <LogoutBtn func={this.logout} />
-						: <Redirect to='/' />
-					}
-				</FlexBox>
+				
+				<Split direction='left' percent='20%'>
+						<h1>This is the left pane</h1>
+				</Split>
+				<Split direction='right' percent='80%'>
+						<h1>You are logged in!</h1>
+						{isLoggedIn
+							? <LogoutBtn func={this.logout} />
+							: <Redirect to='/' />
+						}
+				</Split>
+
+
 			</div>
 		);
 	}
@@ -40,13 +46,18 @@ export default class UserPage extends React.Component {
 }
 
 
-function FlexBox(props) {
-	return(
-		<div className='flexbox'>
+// Split layout t
+function Split(props) {
+
+	const style = {width: props.percent};
+
+	return (
+		<div className={'flexbox split ' + props.direction} style={style}>
 			{props.children}
 		</div>
-	);
+		);
 }
+
 
 function LogoutBtn(props) {
 	return (
---
+++ src/components/user/Sidebar.jsx
@@ -0,0 +1,8 @@
+// ** Main modules
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+// ** Components
+import Login from './Login.js'
+
+// ** CSS
