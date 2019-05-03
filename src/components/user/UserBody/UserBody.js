481519adcc3a863440f6b16810731ee21369afca
Dimitris kastrinakis <dimitriskas.1997@gmail.com>
2019-05-03 18:15:28 +0300

    Added Gallery Showcase component. Banner changes.

--- src/components/user/UserBody/UserBody.js
+++ src/components/user/UserBody/UserBody.js
@@ -18,61 +18,77 @@ export default class UserBody extends React.Component {
         {id: 3, name: 'img3name', url: 'http::/3234' },
         {id: 4, name: 'img4name', url: 'http::/4234' },
         {id: 5, name: 'img5name', url: 'http::/5234' },
+        {id: 6, name: 'img6name', url: 'http::/6234' },
+        {id: 7, name: 'img7name', url: 'http::/7234' },
+        {id: 8, name: 'img8name', url: 'http::/8234' },
+        {id: 9, name: 'img9name', url: 'http::/9234' },
+        {id: 10, name: 'img10name', url: 'http::/10234' },
+        {id: 11, name: 'img11name', url: 'http::/11234' },
       ],
     }
+
+    this.TopBanner = this.TopBanner.bind(this);
+    this.GalleryShowcase = this.GalleryShowcase.bind(this);
+    this.Thumbnails = this.Thumbnails.bind(this);
+    this.Thumbnail = this.Thumbnail.bind(this);
+
   }
+
+  TopBanner() {
+		return (
+			<picture className='top-banner'>
+				<div className='top-banner_image'/>
+				<h1 className='top-banner_title'>Disastergram</h1>
+			</picture>    
+		);
+	}
+
+	GalleryShowcase(photos){
+
+	  let galleryName = 'Gallery Name';
+
+	  return (
+	    <article className='gal-showcase card '>
+	      <h3 className='gal-showcase__title'>
+	        {galleryName}
+	      </h3>
+	      <section className='gal-showcase__body'>
+	        {this.Thumbnails()}
+	      </section>
+	    </article>
+	  );
+	}
+
+	Thumbnails(){
+		const photos = this.state.photos;
+
+	  return photos.map( photo => {
+	    return this.Thumbnail(photo);
+	  });
+	}
+
+	Thumbnail(photo){
+	  return(
+	    <div className='thumbnail'></div>
+	  );
+	}
 
   render() {
     const photos = this.state.photos;
-    // alert(photos)
-    // alert(this.state.photos)
-    // alert(photos[0])
-    alert(typeof photos)
 
     return(
-      <header id='userbody'>
-        {TopBanner()}
-        {GalleryShowcase(this.state.photos)}
-      </header>
+      <div id='userbody'>
+        {this.TopBanner()}
+        <div className='userbody__container'>
+		      {this.GalleryShowcase()}
+		      {this.GalleryShowcase()}
+		      {this.GalleryShowcase()}
+		      {this.GalleryShowcase()}
+        </div>
+      </div>
     );
   }
 
 
 }
 
-function TopBanner() {
-
-  return (
-    <picture className='top-banner'>
-      <div className='top-banner_image'/>
-    </picture>    
-  );
-}
-
-function GalleryShowcase(photos){
-
-  let galleryName = 'Gallery Name';
-
-  return (
-    <article className='gal-showcase'>
-      <h3 className='gal-showcase__title'>
-        {galleryName}
-      </h3>
-      <section className='gal-showcase__body'>
-        <Thumbnails photos={photos}/>
-      </section>
-    </article>
-  );
-}
-
-function Thumbnails(photos){
-  return photos.map( photo => {
-    Thumbnail(photo);
-  });
-}
-
-function Thumbnail(photo){
-  return(
-    <div className='thumbnail'></div>
-  );
-}
