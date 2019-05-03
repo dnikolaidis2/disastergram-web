b5a6ab5f29f110e45d9d9b98c7d6c45b8b281f69
Dimitris Kastrinakis <dimitriskas.1997@gmail.com>
2019-05-03 15:03:15 +0300

    [WIP] userbody changes

--- src/components/user/UserBody/UserBody.js
+++ src/components/user/UserBody/UserBody.js
@@ -2,6 +2,8 @@
 import React from 'react';
 //import { Redirect } from 'react-router-dom';
 
+import './userbody.css';
+
 export default class UserBody extends React.Component {
 
   constructor(props) {
@@ -9,21 +11,68 @@ export default class UserBody extends React.Component {
 
     this.Auth = this.props.Auth;
 
-
+    this.state = {
+      photos: [
+        {id: 1, name: 'img1name', url: 'http::/1234' },
+        {id: 2, name: 'img2name', url: 'http::/2234' },
+        {id: 3, name: 'img3name', url: 'http::/3234' },
+        {id: 4, name: 'img4name', url: 'http::/4234' },
+        {id: 5, name: 'img5name', url: 'http::/5234' },
+      ],
+    }
   }
 
-
-
   render() {
-
+    const photos = this.state.photos;
+    // alert(photos)
+    // alert(this.state.photos)
+    // alert(photos[0])
+    alert(typeof photos)
 
     return(
-      <section>
-        <picture className='top-banner'/>
-        <h1>You are logged in!</h1>
-      </section>
+      <header id='userbody'>
+        {TopBanner()}
+        {GalleryShowcase(this.state.photos)}
+      </header>
     );
   }
 
 
+}
+
+function TopBanner() {
+
+  return (
+    <picture className='top-banner'>
+      <div className='top-banner_image'/>
+    </picture>    
+  );
+}
+
+function GalleryShowcase(photos){
+
+  let galleryName = 'Gallery Name';
+
+  return (
+    <article className='gal-showcase'>
+      <h3 className='gal-showcase__title'>
+        {galleryName}
+      </h3>
+      <section className='gal-showcase__body'>
+        <Thumbnails photos={photos}/>
+      </section>
+    </article>
+  );
+}
+
+function Thumbnails(photos){
+  return photos.map( photo => {
+    Thumbnail(photo);
+  });
+}
+
+function Thumbnail(photo){
+  return(
+    <div className='thumbnail'></div>
+  );
 }
